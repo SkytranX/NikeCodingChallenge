d5c53e684a6104efeca772315bc8117e4344cfa6
Steven Tran <StevenSkytran@gmail.com>
2020-02-13 16:15:09 -0500

    progress spinner added

--- app/src/main/java/com/example/nikecodingchallenge/view/SearchWordFragment.kt
+++ app/src/main/java/com/example/nikecodingchallenge/view/SearchWordFragment.kt
@@ -1,10 +1,14 @@
 package com.example.nikecodingchallenge.view
 
 
+import android.nfc.Tag
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.INVISIBLE
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -14,7 +18,10 @@ import com.example.nikecodingchallenge.R
 import com.example.nikecodingchallenge.adapter.DictionaryAdapter
 import com.example.nikecodingchallenge.extensions.parseDate
 import com.example.nikecodingchallenge.viewmodel.MainViewModel
+import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.fragment_search_word.*
 import kotlinx.android.synthetic.main.fragment_search_word.view.*
+import kotlinx.android.synthetic.main.key_info.view.*
 
 /**
  * A simple [Fragment] subclass.
@@ -32,10 +39,17 @@ class SearchWordFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         adapter = DictionaryAdapter()
         view.apply {
-            wordInput.setText("Jawn")
+            wordInput.setText(" Input word...")
+
             btnSearch.setOnClickListener {
-                viewModel.defineTerm(wordInput.text.toString())
+
+                    progrs_Bar?.visibility = View.VISIBLE
+                    viewModel.defineTerm(wordInput.text.toString())
+                Log.d("TAG","invisible sumbitted")
+
             }
+
+
             recyclerDictionary.apply {
                 layoutManager = LinearLayoutManager(context)
                 adapter = this@SearchWordFragment.adapter
@@ -48,6 +62,7 @@ class SearchWordFragment : Fragment() {
         wordInputObservable()
     }
 
+
     private fun wordInputObservable() {
         viewModel.definitions.observe(this, Observer {
             println("PRINTING DATE FORMAT")
@@ -56,6 +71,8 @@ class SearchWordFragment : Fragment() {
                 "TO -> ${it.list[0].written_on.parseDate()}"
             )
             adapter?.submitList(it.list)
+            progrs_Bar?.visibility = INVISIBLE
+            Log.d("TAG","invisible")
         })
     }
 }
